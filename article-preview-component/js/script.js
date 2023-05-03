d1ff96241abf53079b4844b22132f200f7bc8ca4
aishwarya-mali <aishwaryamali29@gmail.com>
2023-05-03 12:57:56 +0530

    worked on js and responsive css

---
+++ article-preview-component/js/script.js
@@ -0,0 +1,7 @@
+const shareBtn = document.querySelector('.share')
+const shareIcons = document.querySelector('.share-icons')
+
+
+shareBtn.addEventListener('click', function () {
+    shareIcons.style.visibility = "visible"
+})
