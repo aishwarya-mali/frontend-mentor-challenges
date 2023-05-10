d9e3a2f6057b01a19878f141962855edf7daa7f4
aishwarya-mali <aishwaryamali29@gmail.com>
2023-05-10 18:24:42 +0530

    responsive and mobile nav

---
+++ news-homepage/js/script.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById('hamburger-menu')
+const closeMenu = document.getElementById('close-menu')
+const mbMenu = document.getElementById('mb-menu')
+
+menu.addEventListener('click', function(e){
+    menu.style.display = "none"
+    closeMenu.style.display = "inline-block"
+    mbMenu.style.display = "inline-block"
+})
+
+closeMenu.addEventListener('click', function(e){
+    closeMenu.style.display = "none"
+    menu.style.display = "inline-block"
+    mbMenu.style.display = "none"
+})
