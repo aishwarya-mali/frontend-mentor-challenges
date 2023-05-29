9e7010c9d85991970a74dbeda9c5e1e7bf5cf4c4
aishwarya-mali <aishwaryamali29@gmail.com>
2023-05-29 12:55:20 +0530

    added animation when toggling sidebar

--- news-homepage/js/script.js
+++ news-homepage/js/script.js
@@ -6,10 +6,14 @@ menu.addEventListener('click', function(e){
     menu.style.display = "none"
     closeMenu.style.display = "inline-block"
     mbMenu.style.display = "inline-block"
+    mbMenu.style.opacity = "1"
+    mbMenu.style.animation = "fadeIn 0.3s";
 })
 
 closeMenu.addEventListener('click', function(e){
     closeMenu.style.display = "none"
     menu.style.display = "inline-block"
     mbMenu.style.display = "none"
+    mbMenu.style.opacity = "0"
+    mbMenu.style.animation = "";
 })
